dbcd2ad6d436f2e660f51dbdd6deda4bf1e7f6c3
rohit <rohit13bavaliya@gmail.com>
2021-07-15 13:10:32 +0530

    commit

--- app/src/main/java/com/example/videostreamingapp/DashBoard.java
+++ app/src/main/java/com/example/videostreamingapp/DashBoard.java
@@ -1,23 +1,36 @@
 package com.example.videostreamingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DashBoard extends AppCompatActivity {
     FloatingActionButton addVideo;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
 
-        addVideo = (FloatingActionButton) findViewById(R.id.addVideo);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        // fetch data from firebase database
+        FirebaseRecyclerOptions<FileModel> options =
+                new FirebaseRecyclerOptions.Builder<FileModel>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("videos"), FileModel.class)
+                        .build();
+
+        addVideo = (FloatingActionButton) findViewById(R.id.addVideo);
         addVideo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
