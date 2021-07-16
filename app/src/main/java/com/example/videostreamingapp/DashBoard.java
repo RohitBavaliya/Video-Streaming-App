247d46d2764957d61b9967be42be1ee0d131d609
rohit <rohit13bavaliya@gmail.com>
2021-07-16 22:39:13 +0530

    like button implementation

--- app/src/main/java/com/example/videostreamingapp/DashBoard.java
+++ app/src/main/java/com/example/videostreamingapp/DashBoard.java
@@ -14,11 +14,19 @@ import android.view.ViewGroup;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class DashBoard extends AppCompatActivity {
     FloatingActionButton addVideo;
     RecyclerView recyclerView;
+    DatabaseReference likeReference;
+    Boolean testClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +35,10 @@ public class DashBoard extends AppCompatActivity {
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // likes database reference
+        likeReference = FirebaseDatabase.getInstance().getReference("likes");
+
 
         // fetch data from firebase database
         FirebaseRecyclerOptions<FileModel> options =
@@ -42,6 +54,48 @@ public class DashBoard extends AppCompatActivity {
             @Override
             protected void onBindViewHolder(@NonNull  ViewHolder holder, int position, @NonNull FileModel model) {
                 holder.prepareExoplayer(getApplication(),model.getTitle(),model.getVideoUrl());
+
+                // get Current User id
+                FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                String userId = firebaseUser.getUid();
+                String postId = getRef(position).getKey();
+
+                holder.getLikeStatus(postId,userId);
+
+                holder.like.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        testClick = true;
+                        likeReference.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                if(testClick == true)
+                                {
+                                    if(snapshot.child(postId).hasChild(userId))
+                                    {
+                                        // already liked video
+                                        // remove entry
+                                        likeReference.child(postId).removeValue();
+                                        testClick = false;
+                                    }
+                                    else {
+                                        // not liked
+                                        // add entry
+                                        likeReference.child(postId).child(userId).setValue(true);
+                                        testClick = false;
+                                    }
+                                }
+
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+                    }
+                });
             }
 
             @NonNull
--- app/src/main/java/com/example/videostreamingapp/ViewHolder.java
+++ app/src/main/java/com/example/videostreamingapp/ViewHolder.java
@@ -3,6 +3,7 @@ package com.example.videostreamingapp;
 import android.app.Application;
 import android.net.Uri;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,9 +20,18 @@ import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.io.File;
 
 
 public class ViewHolder extends RecyclerView.ViewHolder {
@@ -30,11 +40,16 @@ public class ViewHolder extends RecyclerView.ViewHolder {
     SimpleExoPlayerView simpleExoPlayerView;
     // creating a variable for exoplayer
     SimpleExoPlayer exoPlayer;
+    ImageView like;
+    TextView count;
+    DatabaseReference databaseReference;
 
     public ViewHolder(@NonNull View itemView) {
         super(itemView);
         titleVideo = (TextView) itemView.findViewById(R.id.titleVideo);
         simpleExoPlayerView = itemView.findViewById(R.id.idSimpleExoPlayerVIew);
+        like = (ImageView) itemView.findViewById(R.id.like);
+        count = (TextView) itemView.findViewById(R.id.count);
     }
 
     public void prepareExoplayer(Application application,String videoTitle,String videoUrl)
@@ -61,5 +76,33 @@ public class ViewHolder extends RecyclerView.ViewHolder {
         {
             Toast.makeText(application, "ExoPlayer Crahsed!!"+e.getMessage().toString(), Toast.LENGTH_SHORT).show();
         }
+    }
+
+
+    // like function
+    public void getLikeStatus(final String postId,final String userId) {
+        databaseReference = FirebaseDatabase.getInstance().getReference("likes");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.child(postId).hasChild(userId))
+                {
+                    int count_like = (int) snapshot.child(postId).getChildrenCount();
+                    count.setText(count_like+" likes");
+                    like.setImageResource(R.drawable.like);
+                }
+                else
+                {
+                    int count_like = (int)snapshot.child(postId).getChildrenCount();
+                    count.setText(count_like+" likes");
+                    like.setImageResource(R.drawable.dislike);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
