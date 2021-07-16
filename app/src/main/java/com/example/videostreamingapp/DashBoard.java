1d8731281df1b6523de178fc8585098d5673f5da
rohit <rohit13bavaliya@gmail.com>
2021-07-16 12:22:33 +0530

    Set Video in RecyclerView

--- app/src/main/java/com/example/videostreamingapp/DashBoard.java
+++ app/src/main/java/com/example/videostreamingapp/DashBoard.java
@@ -1,13 +1,17 @@
 package com.example.videostreamingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.FirebaseDatabase;
@@ -30,6 +34,24 @@ public class DashBoard extends AppCompatActivity {
                         .setQuery(FirebaseDatabase.getInstance().getReference().child("videos"), FileModel.class)
                         .build();
 
+
+        // creating Firebase Recycler Adapter
+
+        FirebaseRecyclerAdapter<FileModel,ViewHolder> adapter =
+                new FirebaseRecyclerAdapter<FileModel, ViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull  ViewHolder holder, int position, @NonNull FileModel model) {
+                holder.prepareExoplayer(getApplication(),model.getTitle(),model.getVideoUrl());
+            }
+
+            @NonNull
+            @Override
+            public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_row_design,parent,false);
+                return new ViewHolder(view);
+            }
+        };
+
         addVideo = (FloatingActionButton) findViewById(R.id.addVideo);
         addVideo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,5 +59,8 @@ public class DashBoard extends AppCompatActivity {
                 startActivity(new Intent(DashBoard.this,AddVideo.class));
             }
         });
+
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
     }
 }
